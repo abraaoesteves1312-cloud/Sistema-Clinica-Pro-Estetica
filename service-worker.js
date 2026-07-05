1a63dc66ff3e9d517fe2ef7998f1dd6dcd2b9ea7
abraaoesteves1312-cloud <abraaoesteves1312@gmail.com>
2026-07-05 14:04:03 -0300

    Sistema-Clinica-Pro-Estetica

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gestao-estetica-pro-app-v6-20260705-online-scoped-user";
+const CACHE_NAME = "gestao-estetica-pro-app-v7-20260705-login-switch-online-scope";
 const CORE_ASSETS = [
   "./",
   "./index.html",

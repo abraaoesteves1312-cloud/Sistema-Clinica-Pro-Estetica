2933014f37c83ef33c022389c4ae2a5d6903cc41
abraaoesteves1312-cloud <abraaoesteves1312@gmail.com>
2026-07-07 17:42:42 -0300

    Força login online para clínicas

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gestao-estetica-pro-app-v19-20260707-modo-local-explicito";
+const CACHE_NAME = "gestao-estetica-pro-app-v11-20260707-fechamento-despesas";
 const CORE_ASSETS = [
   "./",
   "./index.html",
